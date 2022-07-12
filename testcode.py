3e21adf814a4fcd00840b658bbbc77292043595f
tabonm727@gmail.com <tabonm727@gmail.com>
2022-07-13 02:24:37 +0900

    fix testcode

--- testcode.py
+++ testcode.py
@@ -8,6 +8,8 @@ def test_func(a: int, b: int) -> int:
 def test_func2(a: list, b: int) -> int:
     return sum(a) * b
 
+
 if __name__ == '__main__':
     print(test_func(1, 2))
     print(test_func2([1, 2], 3))
+    
